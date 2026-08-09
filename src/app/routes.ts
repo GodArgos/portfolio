5aede069979f6e145c4d5e12ca2c5ed16a3a3e92
Joaquin Incio <joaquin.incio2002@outlook.com>
2026-08-08 23:32:42 -0500

    Fixed main route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,15 +4,21 @@ import { Home } from "./Home";
 import { ProjectDetail } from "./ProjectDetail";
 import { ProfessionalProjectDetail } from "./ProfessionalProjectDetail";
 import { NotFound } from "./NotFound";
-export const router = createBrowserRouter([
+
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Root,
+      children: [
+        { index: true, Component: Home },
+        { path: "project/:slug", Component: ProjectDetail },
+        { path: "professional/:slug", Component: ProfessionalProjectDetail },
+        { path: "*", Component: NotFound },
+      ],
+    },
+  ],
   {
-    path: "/",
-    Component: Root,
-    children: [
-      { index: true, Component: Home },
-      { path: "project/:slug", Component: ProjectDetail },
-      { path: "professional/:slug", Component: ProfessionalProjectDetail },
-      { path: "*", Component: NotFound },
-    ],
-  },
-]);
+    basename: import.meta.env.BASE_URL,
+  }
+);
